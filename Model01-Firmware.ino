be4b6ce340de8199ea0aa1fc703cb235c06343e7
Daniel Bergey <bergey@teallabs.org>
2017-08-11 18:40:39 -0700

    move some punctuation for Dvorak
    When the OS is set to Dvorak, the stock Keyboardio layout is
    inconvenient for a few characters.  Move []/\= for this case.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -81,8 +81,25 @@ ___ \
     Key_Keymap1_Momentary \
 )
 
+// Move punctuation for the case when OS is set to Dvorak
+#define QWERTY_DVORAK_STYLE KEYMAP_STACKED ( \
+    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
+    Key_Minus,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_Equals, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
+    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
+                          Key_Keymap1_Momentary,     \
+\
+    Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_RightBracket, \
+    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_LeftBracket,       \
+                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
+    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Backslash,       \
+    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,                   \
+    Key_Keymap1_Momentary \
+)
+
 const Key keymaps[][ROWS][COLS] PROGMEM = {
-  QWERTY,
+  QWERTY_DVORAK_STYLE,
   GENERIC_FN2,
   NUMPAD
 };

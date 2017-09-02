fc3de7a573884f60f19a075bdc1c060ac02a5edf
Daniel Bergey <bergey@teallabs.org>
2017-09-02 16:37:23 -0400

    base layer with mods for MacOS

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -83,6 +83,23 @@ ___ \
     Key_Keymap3_Momentary \
 )
 
+// When OS is set to Dvorak, and I have mapped mods for emacs
+#define MAC_LAPTOP KEYMAP_STACKED ( \
+    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
+    Key_Minus,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_Equals, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftGui,        \
+    Key_LeftControl, Key_Backspace, Key_LeftAlt, Key_LeftShift,         \
+                          Key_Keymap3_Momentary,     \
+\
+    Key_Keymap5_Momentary,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_RightBracket, \
+    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_LeftBracket,       \
+                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
+    Key_Escape,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Backslash,       \
+    Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,                   \
+    Key_Keymap3_Momentary \
+)
+
 // Move punctuation back for the case when OS is set to Qwerty
 #define QWERTY_PUNCTUATION KEYMAP_STACKED ( \
     ___,          ___, ___, ___, ___, ___, ___, \
@@ -133,7 +150,7 @@ ___ \
     ___ \
 )
 const Key keymaps[][ROWS][COLS] PROGMEM = {
-  LINUX_LAPTOP,
+  MAC_LAPTOP,
   QWERTY_PUNCTUATION,
   STOCK_MODS,
   GENERIC_FN2,
